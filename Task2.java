173a41636f87d3c72163836e062e1240b38a03b8
sergeimsg <98679437+sergeimsg@users.noreply.github.com>
2022-08-02 01:52:56 +0300

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,17 @@
+/*
+
+Задание 3
+ */
+public class Task2 {
+    public static void main(String[] args) {
+
+
+        int a = 3;
+        int b = 4;
+
+        double sq = a * b / 2;
+        double c = Math.sqrt(a * a + b * b);
+        double per = a+b+c;
+        System.out.println("Square " + sq + "  perim  " + per );
+    }
+}
